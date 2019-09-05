100fa6a943941bc4a41e38e07fb67fdb4cef5b08
qtngo <tuannq265@gmail.com>
2019-09-05 14:59:31 +0700

    add functionality:  + Generate BaseController

---
+++ generators/app/templates/webapp/controller/BaseController.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"<%= namespace.replace(/\./g, '/')%>/<%=projectname.replace('.', '/')%>/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("<%=namespace%>.<%=projectname%>.Component", {
+
+		/*Convenience method generator */
+	});
+});
